f3b0481b862d248589d55d1fd006a455cdb69be3
behrami <behram1993@gmail.com>
2023-02-05 20:07:40 -0500

    added view model with automapper for the leavetype index

---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        public string Name { get; set; }
+
+        [Display(Name= "Default Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
